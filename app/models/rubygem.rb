e7035e921e468dba515c676405c413104139bb2b
willnet <netwillnet@gmail.com>
2016-03-28 23:08:56 +0900

    annotate

--- app/models/rubygem.rb
+++ app/models/rubygem.rb
@@ -1,2 +1,32 @@
+# ## Schema Information
+#
+# Table name: `rubygems`
+#
+# ### Columns
+#
+# Name                     | Type               | Attributes
+# ------------------------ | ------------------ | ---------------------------
+# **`id`**                 | `integer`          | `not null, primary key`
+# **`name`**               | `string`           | `not null`
+# **`version`**            | `string`           | `not null`
+# **`authors`**            | `string`           |
+# **`project_uri`**        | `string`           |
+# **`gem_uri`**            | `string`           |
+# **`homepage_uri`**       | `string`           |
+# **`wiki_uri`**           | `string`           |
+# **`documentation_uri`**  | `string`           |
+# **`mailing_list_uri`**   | `string`           |
+# **`source_code_uri`**    | `string`           |
+# **`bug_tracker_uri`**    | `string`           |
+# **`licenses`**           | `text`             | `default("{}"), is an Array`
+# **`created_at`**         | `datetime`         | `not null`
+# **`updated_at`**         | `datetime`         | `not null`
+#
+# ### Indexes
+#
+# * `index_rubygems_on_name`:
+#     * **`name`**
+#
+
 class Rubygem < ApplicationRecord
 end
