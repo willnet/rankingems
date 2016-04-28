69c66c1dfdf6ecca8ae2ed87d0ca221ff1d775de
willnet <netwillnet@gmail.com>
2016-04-28 13:38:49 +0900

    Add routes for github login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+  get '/auth/:provider/callback' => 'auths#create'
+  get '/auth/failure' => 'auths#failure'
+  get '/logout' => 'auths#destroy', as: :logout
 end
