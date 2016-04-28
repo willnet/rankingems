867be6af59cc233142833994bfea3173a227d172
willnet <netwillnet@gmail.com>
2016-04-29 08:22:05 +0900

    Supress generating assets and helper files

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module Rankingems
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
   end
 end
