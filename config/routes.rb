90a371b1f82e53fc3e92193707abc575d87837ca
willnet <netwillnet@gmail.com>
2016-05-02 09:20:06 +0900

    Enable Sign in & Sign out at navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/auth/failure' => 'sessions#failure'
-  get '/logout' => 'sessions#destroy', as: :logout
+  get '/signout' => 'sessions#destroy', as: :logout
 end
