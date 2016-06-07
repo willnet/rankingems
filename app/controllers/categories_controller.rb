1dec9090dabd79496acd0daf7d1173b264ab0330
willnet <netwillnet@gmail.com>
2016-06-07 18:50:29 +0900

    Enable use friendly id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find(params[:id])
+    @category = Category.friendly.find(params[:id])
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -21,5 +21,8 @@
 #
 
 class Category < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   has_many :rubygems
 end
