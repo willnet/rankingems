5b6f45cd090d803b47095471859bc700463c4b20
willnet <netwillnet@gmail.com>
2016-06-09 22:56:06 +0900

    Order by download count

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
+    @rubygems = @category.rubygems.order(download_count_for_ranking: :desc)
   end
 end
