f6e96272decbc34a8e83629dae2257fa3fc76554
willnet <netwillnet@gmail.com>
2016-06-10 09:24:04 +0900

    Sort by ranking

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
-    @rubygems = @category.rubygems.order(download_count_for_ranking: :desc)
+    @rubygems = @category.rubygems.order(ranking: :desc)
   end
 end
