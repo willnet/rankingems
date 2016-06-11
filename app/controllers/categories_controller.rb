74ec3ea5c783964a3f8a7671a449f4c7bf9d744b
willnet <netwillnet@gmail.com>
2016-06-11 11:13:21 +0900

    Sort ranking asc

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
-    @rubygems = @category.rubygems.order(ranking: :desc)
+    @rubygems = @category.rubygems.order(:ranking)
   end
 end
