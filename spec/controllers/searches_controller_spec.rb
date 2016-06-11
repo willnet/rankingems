c5a81d19e47fd262dfa5fe4c2d81884249d1062f
willnet <netwillnet@gmail.com>
2016-06-11 11:28:46 +0900

    Fix search_controller to searches_controller

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe SearchController, :type => :controller do
+RSpec.describe SearchesController, :type => :controller do
 
   describe "GET index" do
     it "returns http success" do
