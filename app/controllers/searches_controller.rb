4b3b89b627b6ed34b631d56c5f8860cc32dbcbf1
willnet <netwillnet@gmail.com>
2016-06-12 09:00:19 +0900

    Enable search gems and categories

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,7 @@
 class SearchesController < ApplicationController
-  def index
+  def show
+    name = params[:name]
+    @categories = Category.where('name LIKE ?', "%#{name}%").limit(30)
+    @rubygems = Rubygem.where('name LIKE ?', "%#{name}%").preload(:category).limit(30)
   end
 end
