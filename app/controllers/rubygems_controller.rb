303e1aaa719f720de1ca947bd7c531c9728cfbd1
willnet <netwillnet@gmail.com>
2016-06-14 10:29:05 +0900

    Fix error with friendly_id

--- app/controllers/rubygems_controller.rb
+++ app/controllers/rubygems_controller.rb
@@ -2,11 +2,11 @@ class RubygemsController < ApplicationController
   before_action :authenticate, only: :edit
 
   def show
-    @rubygem = Rubygem.find(params[:id])
+    @rubygem = Rubygem.friendly.find(params[:id])
   end
 
   def edit
-    @rubygem = Rubygem.find(params[:id])
+    @rubygem = Rubygem.friendly.find(params[:id])
     @suggestion = @rubygem.suggestions.build
   end
 end
