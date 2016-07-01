7bc32c8887ecf9aadebfe95b97a6f5f2381fde1e
willnet <netwillnet@gmail.com>
2016-07-01 18:22:11 +0900

    Add rubygem pages for admin

--- app/controllers/admin/rubygems_controller.rb
+++ app/controllers/admin/rubygems_controller.rb
@@ -2,4 +2,8 @@ class Admin::RubygemsController < Admin::BaseController
   def index
     @rubygems = Rubygem.page(params[:page]).per(20).all
   end
+
+  def show
+    @rubygem = Rubygem.friendly.find(params[:id])
+  end
 end
