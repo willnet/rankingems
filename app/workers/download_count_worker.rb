bf0d5df6e7a7c66d74ab7da9cdecf93e7a718361
willnet <netwillnet@gmail.com>
2016-07-16 15:30:24 +0900

    Enable log in DownloadCountWorker

--- app/workers/download_count_worker.rb
+++ app/workers/download_count_worker.rb
@@ -4,6 +4,7 @@ class DownloadCountWorker
   def perform
     # TODO: How do we get all gems info with 1 second interval?
     Rubygem.find_each do |rubygem|
+      logger.info "[save_total_download_count]rubygem_id: #{rubygem.id}"
       rubygem.save_total_download_count!
       sleep 1
     end
