3292241b64488322018100ff6f6c3ddd3e818e6c
willnet <netwillnet@gmail.com>
2016-07-27 22:55:40 +0900

    Change api response

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,7 +1,7 @@
 class Api::CategoriesController < ApplicationController
   def index
-    category_names = Category.where('name LIKE ?', "%#{params[:q]}%")
-                             .pluck(:name)
-    render json: category_names
+    category_names = Category.where('name LIKE ?', "%#{params[:q]}%").select(:id, :name)
+
+    render json: category_names.map { |c| { id: c.id,  name: c.name } }
   end
 end
